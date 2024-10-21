716051d17176d7c17aa489b305f917c88364212c
brunobahri <brunobahri27@gmail.com>
2024-10-20 21:57:44 -0300

    first commit: just coding for hobby

---
+++ exibe_mensagem.py
@@ -0,0 +1,36 @@
+import time
+
+def exibir_mensagem():
+    # Exibe a mensagem de "carregando" por 5 segundos
+    print("Acessando informações de mensagem do Grupo jorge&matheus....")
+    time.sleep(5)  # Aguarda 5 segundos
+    
+    # Mensagem fixa com indentação correta
+    mensagem = """
+1. Heloise ✅  
+2. Faberson ✅  
+3. Bia ✅  
+4. Carol ✅  
+   • Carolina Ramirez Kosop  
+   • CPF: 119.708.189-56  
+5. Guilherme ✅  
+   • CPF: 052.627.389-58  
+   • Endereço: Rua Jacob Wellner, 290 - Vista Alegre  
+6. Vitória ✅  
+7. Bruno Sergio Bahri Luiz ✅  
+   • CPF: 111.500.209-03  
+8. Lis  
+9. Charles  
+10. Gaby  
+11. Rhaissa Machado Rodrigues ✅  
+   • CPF: 485.094.088-90  
+12. Wellington Chaves de Souza ✅  
+   • CPF: 432.348.708-88  
+13. Adrilan Geize Ribeiro Barreto ✅  
+   • CPF: 023.407.082-06  
+14. Luane ✅
+    """
+    print(mensagem)
+
+# Chama a função para exibir a mensagem
+exibir_mensagem()
